6ecd0d73575568f914483dbb45fe8511141033ff
Arnaldo Garcia <asgar.2792@gmail.com>
2020-01-19 14:23:35 -0800

    fix: making db public accessible and removing root user from template

--- spec/quetzal_db/cfn/secrets_manager/root_secret_spec.rb
+++ spec/quetzal_db/cfn/secrets_manager/root_secret_spec.rb
@@ -1,19 +1,37 @@
 # frozen_string_literal: true
 
-RSpec.describe QuetzalDb::Cfn::SecretsManager::RootSecret do
+# rubocop:disable RSpec/DescribeClass
+RSpec.describe 'QuetzalDb::Cfn::SecretsManager::RootSecret' do
+  # rubocop:enable RSpec/DescribeClass
+
   subject(:template) do
     Helpers::Template.new(File.join(__dir__, '..', '..', '..', '..', 'quetzal-db-update-stack.yml'))
   end
 
   let(:secret) { template['Resources']['QuetzalDbRootSecret'] }
 
-  it_behaves_like 'secret'
+  let(:expected_secrets_template) do
+    {
+      'Fn::Join' => [
+        '',
+        [
+          '{"username": "',
+          {
+            'Ref' => 'RootUser'
+          },
+          '"}'
+        ]
+      ]
+    }
+  end
 
   it 'sets the correct description' do
     expect(secret['Properties']['Description']).to eq('Quetzal DB Root Access')
   end
 
   it 'sets the correct string template' do
-    expect(secret['Properties']['GenerateSecretString']['SecretStringTemplate']).to eq('{"username": "root"}')
+    expect(secret['Properties']['GenerateSecretString']['SecretStringTemplate']).to eq(expected_secrets_template)
   end
+
+  it_behaves_like 'secret'
 end
